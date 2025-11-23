2bf27e3d516374a1305b35a2b58eebc96ec39049
Artemova Dina <artemova@perx.ru>
2025-11-23 14:10:41 +0300

    add lessons list to cours

--- materials/serializer.py
+++ materials/serializer.py
@@ -4,9 +4,18 @@ from rest_framework.serializers import ModelSerializer
 from materials.models import Course, Lesson
 
 
+class LessonSerializer(ModelSerializer):
+    """Сериализатор для уроков курсов обучения"""
+
+    class Meta:
+        model = Lesson
+        fields = '__all__'
+
+
 class CourseSerializer(ModelSerializer):
     """Сериализатор для класса курсов обучения"""
     lessons_count = SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True)
 
     def get_lessons_count(self, course):
         """Метод получения количества уроков курса"""
@@ -14,12 +23,4 @@ class CourseSerializer(ModelSerializer):
 
     class Meta:
         model = Course
-        fields = ('id', 'name', 'description', 'image', 'lessons_count', 'created_at', 'updated_at',)
-
-
-class LessonSerializer(ModelSerializer):
-    """Сериализатор для уроков курсов обучения"""
-
-    class Meta:
-        model = Lesson
-        fields = '__all__'
+        fields = ('id', 'name', 'description', 'image', 'lessons_count', 'lessons', 'created_at', 'updated_at',)
