29df50e1742752b87a5d7a5f73ac3e1957d4463e
Artemova Dina <artemova@perx.ru>
2026-02-04 20:35:34 +0300

    add ci/cd workflow

--- materials/validators.py
+++ materials/validators.py
@@ -6,6 +6,6 @@ from django.core.exceptions import ValidationError
 def lesson_link_validator(value):
     """Валидатор проверяет, что ссылка на урок ведет исключительно на youtube"""
 
-    reg = re.compile("https:\/\/youtu.be\/[A-Za-z]+")
+    reg = re.compile("https://youtu.be/[A-Za-z]+")
     if not bool(reg.match(value)):
         raise ValidationError("You can use youtube link only")
